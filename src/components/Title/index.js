689477bdb89643ada3c6509df674e0fd92100c84
Tayfun Sarıgül <tayfunsarigul@Tayfun-MacBook-Pro.local>
2023-08-13 15:01:43 +0300

    Set up React Native Project

---
+++ src/components/Title/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text } from 'react-native';
+import styles from './styles';
+
+const Title = (props) => {
+    console.log('props :>>', props);
+
+    return (
+
+        <Text style={styles.title}>{props.text}</Text>
+
+    );
+};
+
+Title.defaultProps = {
+    text: 'Default Text'
+};
+
+export default Title;
---
+++ src/screens/Home/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { SafeAreaView, View } from 'react-native';
+import Title from '../../components/Title';
+
+const Home = () => {
+
+    return (
+        <SafeAreaView>
+        <View>
+        <Title text='My Fşrst Conponent'/>
+        <Title/>
+        </View>
+        </SafeAreaView>
+
+    );
+};
+
+export default Home;
